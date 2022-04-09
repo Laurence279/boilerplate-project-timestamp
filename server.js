ff39f77b4737ffee5889b560e43faac19b70b18d
Laurence279 <lnunn279@gmail.com>
2022-04-10 00:35:51 +0100

    change port

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ function getTimestampObject(date)
 
 
 // listen for requests :)
-var listener = app.listen(listener.address(), function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
