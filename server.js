e16d4a6ccc1ce3ad72c9807ca8c7633a7510ac6a
Laurence279 <lnunn279@gmail.com>
2022-04-10 00:30:13 +0100

    Add handlers

--- server.js
+++ server.js
@@ -18,15 +18,42 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api", function (req, res) {
+  const date = new Date();
+  const timestamp = getTimestampObject(date);
+  res.json(timestamp);
+
+})
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  
+  const date = new Date(isNaN(req.params.date) ? req.params.date : Number(req.params.date));
+  
+  const timestamp = getTimestampObject(date);
+
+  if(timestamp.unix == null || timestamp.utc == "Invalid Date") 
+  {
+    res.json({error: "Invalid Date"})
+    return;
+  }
+  res.json(timestamp);
 });
+
+function getTimestampObject(date)
+{
+  const unix = Date.parse(date);
+  const utc = date.toUTCString();
+  const timestamp = {
+    unix: unix,
+    utc: utc
+  }
+  return timestamp;
+}
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
